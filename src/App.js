fbfd3388e2ddafb3eb3b0265b72d4c846aa65555
Ramiro Boza <ramiro.boza@hotmail.com>
2023-02-13 11:14:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import { Table, Space, Button } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+
 
 function App() {
+  const deleteItem = async (id) => {
+    console.log(id)
+
+    const esto = await fetch(`http://localhost:5000/items/${id}`, {
+      method: 'DELETE',
+    }).then(response => response.json())
+    console.log('esto', esto)
+  }
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Id',
+      dataIndex: '_id',
+      key: '_id',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Actions',
+      key: 'action',
+      render: (_, record) => (
+        <Button onClick={() => deleteItem(record._id)}>
+          {"Delete"}
+        </Button>
+      ),
+    },
+  ];
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:5000/items')
+      setItems(await response.json())
+    }
+    fetchData()
+      .catch(console.error)
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table dataSource={items} columns={columns} />;
     </div>
   );
 }
